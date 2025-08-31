963e717dc5021bd2adc15bb84607684a2f701963
davidlealxyz <davidleal.xyz@egmail.com>
2025-08-31 14:52:28 -0500

    index working fine

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,6 +1,7 @@
 AUTHOR = 'David Lemus'
 SITENAME = "Jesus Lemus"
 SITETITLE = "Jesus Lemus - Platform Engineer"
+SITESUBTITLE = "Platform Engineer"
 SITEDESCRIPTION = "A blog about technology and more."
 SITEURL = ""
 
@@ -14,18 +15,22 @@ FEED_ALL_ATOM = 'feeds/all.atom.xml'
 
 # Navigation menu
 MENUITEMS = (
-    ('Blog', '/'),
+    ('Blog', '/blog/'),
     ('About', '/pages/about.html'),
     ('Projects', '/pages/projects.html'),
 )
 
 # Social widget
+GITHUB_USERNAME = 'your-username'
 SOCIAL = (
-    ('GitHub', 'https://github.com/your-username'),
+    ('GitHub', f'https://github.com/{GITHUB_USERNAME}'),
     ('LinkedIn', 'https://linkedin.com/in/your-username'),
     ('Email', 'mailto:jesus.lemus.leal@gmail.com'),
 )
 
+# Tell Pelican to use a page for the homepage and move the article list
+INDEX_SAVE_AS = 'blog.html' # /themes/simple-dev/templates/index.html
+
 # URL settings for static pages
-PAGE_URL = 'pages/{slug}.html'
-PAGE_SAVE_AS = 'pages/{slug}.html'
+PAGE_URL = '{slug}.html'
+PAGE_SAVE_AS = './{slug}.html'
